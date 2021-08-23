13a6554bb1b01cf92608253f610d8096df922f10
Doug Frei <dougfrei@gmail.com>
2021-08-23 14:25:12 -0700

    update default gravity forms filters

--- bootstrap/plugins.php
+++ bootstrap/plugins.php
@@ -17,27 +17,7 @@ add_filter('wpseo_metabox_prio', function () {
 // ********************
 // Gravity Forms
 // ********************
-add_filter('gform_init_scripts_footer', '__return_true');
+// add_filter('gform_init_scripts_footer', '__return_true');
 
-// Wrap the inline JS that Gravity Forms outputs with an event
-// listener that fires after the page has been loaded.
-// This ensures that jQuery is available even if it has been
-// deferred. Without this hook, jQuery must be loadeded in the
-// <head> and cannot be deferred.
-add_filter('gform_cdata_open', function ($js) {
-	// phpcs:ignore
-	if ((defined('DOING_AJAX') && DOING_AJAX) || isset($_POST['gform_ajax'])) {
-		return $js;
-	}
-
-	return "document.addEventListener('DOMContentLoaded', function() { ";
-});
-
-add_filter('gform_cdata_close', function ($js) {
-	// phpcs:ignore
-	if ((defined('DOING_AJAX') && DOING_AJAX) || isset($_POST['gform_ajax'])) {
-		return $js;
-	}
-
-	return ' });';
-});
+// WPUtil\Vendor\GravityForms::move_scripts_to_footer();
+// WPUtil\Vendor\GravityForms::safely_output_inline_scripts();
